be5b1df17044d8fc0ab329fb98804837c0ac3089
Garrett van Wageningen <ghvanw283@gmail.com>
2020-10-24 15:42:36 -0500

    save work

--- src/graph.ts
+++ src/graph.ts
@@ -34,29 +34,80 @@ type Graph<A> = {
     edges: Set<Record<Edge<A> | WeightedEdge<A>>> // TODO - might not work
 }
 
-type AdjacencyMap<A> = {
-    map: Map<Record<A>, Set<Record<Edge<A>>>>;
+type AdjacencyMap<A> = Map<Record<A>, Set<Record<Edge<A>>>>;
+
+type WeightedAdjacencyMap<A> = Map<Record<A>, Set<Record<WeightedEdge<A>>>>;
+
+interface IInventory<A> {
+    peek(): A;
+    pop(): IInventory<A>;
+    conj(item: A): IInventory<A>;
 }
 
-type WeightedAdjacencyMap<A> = {
-    map: Map<Record<A>, Set<Record<WeightedEdge<A>>>>;
+function simpleStack<A>(...items: Array<A>): IInventory<A> {
+    return {
+        peek(): A {
+            return items[items.length - 1];
+        },
+        pop(): IInventory<A> {
+            items.pop();
+            return simpleStack(...items);
+        },
+        conj(item: A): IInventory<A> {
+            items.push(item);
+            return simpleStack(...items);
+        },
+    }
 }
 
+function simpleQueue<A>(...items: Array<A>): IInventory<A> {
+    return {
+        peek(): A {
+            return items[0];
+        },
+        pop(): IInventory<A> {
+            items.shift();
+            return simpleStack(...items);
+        },
+        conj(item: A): IInventory<A> {
+            items.push(item);
+            return simpleStack(...items);
+        },
+    }
+}
+
+interface TraverseEnv<A> {
+    visited: Set<Record<A>>;
+    inventory: IInventory<Record<A>>;
+}
 
 function* traverse<A>(
     map: AdjacencyMap<A>, 
-    visited: Set<Record<A>>, 
-    memory: Collection.Indexed<A>): Generator<Record<A>, void, void> {
+    { visited, inventory }: TraverseEnv<A>): Generator<Record<A>, void, void> {
 
+    const next = inventory.peek();
+    if (next === undefined) {
+        return;
+    }
+
+    const newInventory =
+        map
+            .get(next)
+            .reduce((agg, edge) => {
+                if (edge.to)
+            }, { visited, inventory });
+
+    yield next;
+    yield* traverse(map, visited, )
 }
 
-function* depthFirstTraverse<A>(map: AdjacencyMap<A>): Generator<> {
+// function* depthFirstTraverse<A>(map: AdjacencyMap<A>): Generator<> {
 
-}
+// }
 
-function* breadthFirstTraverse<A>(map: AdjacencyMap<A>): Generator<> {
+// function* breadthFirstTraverse<A>(map: AdjacencyMap<A>): Generator<> {
 
-}
+// }
 
 
 
